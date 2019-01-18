536f8f0d2df685d61c0f1964e35f15e260f03bc9
Nolan Glenn <nolangglenn@gmail.com>
2019-01-18 14:51:26 -0600

    Update app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+// Declare and store possible trivia questions and their respective answers
+var questions = [
+  {
+    question: "What company did George claim to interview for as a latex salesman?",
+    answers: {
+      a: "Costanza and Sons",
+      b: "Vandelay Industries",
+      c: "Latex Industries"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Which Seinfeld writer voiced George Steinbrenner on and off from seasons five through nine?",
+    answers: {
+      a: "Larry David",
+      b: "Peter Mehlman",
+      c: "Spike Feresten"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Kramer's apartment is flooded with red light from this restaurant's neon sign:",
+    answers: {
+      a: "KFC",
+      b: "Kenny Roger's Roasters",
+      c: "The Diner"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What does Elaine ask George to order for her from Monk's?",
+    answers: {
+      a: "Egg Whites and Toast",
+      b: "Tuna on Toast",
+      c: "Big Salad"
+    },
+    correctAnswer: "c"
+  }];
+  
+  
+  
+  
+  
