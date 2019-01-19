28fb15c33c7ece426258e4d7584d2b2f6c2892bf
nolanglenn <nolangglenn@gmail.com>
2019-01-19 00:35:03 -0600

    Added jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 // Declare and store possible trivia questions and their respective answers
-var questions = [
+const quizContainer = $("#trivia");
+const submitButton = $("#submit");
+const resultsContainer = $("#results");
+
+const questions = [
   {
     question: "What company did George claim to interview for as a latex salesman?",
     answers: {
@@ -35,9 +39,67 @@ var questions = [
       c: "Big Salad"
     },
     correctAnswer: "c"
-  }];
-  
-  
-  
-  
-  
+}];
+
+function beginTrivia(){
+
+    $("#trivia").css('display', 'block');
+
+    const output = [];
+
+
+    questions.forEach(
+        (currentQuestion, questionNumber) => {
+
+        const answers = [];
+
+        for(letter in currentQuestion.answers){
+
+            answers.push(
+            `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+            </label>`
+            );
+        }
+
+        output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+        );
+        }
+    );
+
+    // quizContainer.innerHTML = output.join('');
+    $("#trivia").append(output);
+};
+
+function showResults(){
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    questions.forEach( (currentQuestion, questionNumber) => {
+
+    const answerContainer = answerContainers[questionNumber];
+    const selector = 'input[name=question'+questionNumber+']:checked';
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    if(userAnswer===currentQuestion.correctAnswer){
+    numCorrect++;
+
+    answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    else{
+    answerContainers[questionNumber].style.color = 'red';
+    }
+    });
+
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+};
+
+$(".begin").on("click", beginTrivia());
+
+$("#submit").on("click", showResults);
